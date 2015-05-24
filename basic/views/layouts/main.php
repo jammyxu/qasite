7f3f40f46198386ca858af33701318717d0c60e2
jimmyxu <jimmyxu@anjuke.com>
2015-05-24 23:04:32 +0800

    renew main.php

--- basic/views/layouts/main.php
+++ basic/views/layouts/main.php
@@ -26,25 +26,60 @@ AppAsset::register($this);
     <div class="wrap">
         <?php
             NavBar::begin([
-                'brandLabel' => 'My Company',
+                'brandLabel' => '银天下QA',
                 'brandUrl' => Yii::$app->homeUrl,
                 'options' => [
                     'class' => 'navbar-inverse navbar-fixed-top',
                 ],
             ]);
             echo Nav::widget([
-                'options' => ['class' => 'navbar-nav navbar-right'],
                 'items' => [
-                    ['label' => 'Home', 'url' => ['/site/index']],
-                    ['label' => 'About', 'url' => ['/site/about']],
-                    ['label' => 'Contact', 'url' => ['/site/contact']],
-                    Yii::$app->user->isGuest ?
-                        ['label' => 'Login', 'url' => ['/site/login']] :
-                        ['label' => 'Logout (' . Yii::$app->user->identity->username . ')',
-                            'url' => ['/site/logout'],
-                            'linkOptions' => ['data-method' => 'post']],
+                    [
+                        'label' => 'Home',
+                        'url' => ['site/index'],
+                    ],
+                    [
+                        'label' => 'Dropdown',
+                        'items' => [
+                             ['label' => 'Level 1 - Dropdown A', 'url' => '#'],
+                             '<li class="divider"></li>',
+                             '<li class="dropdown-header">Dropdown Header</li>',
+                             ['label' => 'Level 1 - Dropdown B', 'url' => '#'],
+                        ],
+                    ],
                 ],
-            ]);
+                'options' => ['class' =>'nav-pills navbar-right'], // set this to nav-tab to get tab-styled navigation
+            ]);            
+            // echo Nav::widget([
+            //     'options' => ['class' => 'navbar-nav navbar-right'],
+            //     'items' => [
+            //         [
+            //         'label' => '报告',
+            //         'items' => [
+            //                 ['label' => 'Android报告', 'url' => ['/site/index']],
+            //                 ['label' => 'IOS报告', 'url' => ['/site/index']],
+            //             ],    
+            //         ],
+            //         [
+            //         'label' => '工具', 
+            //         'items' => [
+            //                 ['label' => '注册清理', 'url' => ['/site/about']],
+            //                 ['label' => '开户清理', 'url' => ['/site/about']],
+            //             ],                   
+            //         ],
+            //         [
+            //         'label' => '学习', 
+            //         'items' => [
+            //                 ['label' => '新人', 'url' => ['/site/contact']],
+            //             ],                    
+            //         ],
+            //         Yii::$app->user->isGuest ?
+            //             ['label' => 'Login', 'url' => ['/site/login']] :
+            //             ['label' => 'Logout (' . Yii::$app->user->identity->username . ')',
+            //                 'url' => ['/site/logout'],
+            //                 'linkOptions' => ['data-method' => 'post']],
+            //     ],
+            // ]);
             NavBar::end();
         ?>
 
